736acc0dad4058eeb1713ba3dd2779e2e36fb41f
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-02-28 20:16:04 +0900

    updated with reference

--- main.cpp
+++ main.cpp
@@ -12,52 +12,55 @@ vector<char> conv = {'I','X','W'};
 
 vector<vector<char>> buffer(width, vector<char>(height));
 
-void chara (vector<vector<char>> s);
-void charainit (void);
-void convert (vector<vector<int>> bufint);
+void charainit (vector<vector<char>> &bufc);
+void chara (vector<vector<char>> &bufc);
+void convert (vector<vector<int>> &bufint, vector<vector<char>> &bufc);
 
 int main(void){
 
 	for(int i = 0; i<frame; i++){
 
-		charainit();
+		charainit(buffer);
 
 		vector<vector<int>> bufint (width, vector<int>(height));
 		for(int j = 0; j<height; j++){
 			bufint.at(i%width).at(j) = i%3;
 		}
-		convert(bufint);
+		convert(bufint,buffer);
 		chara(buffer);
-
 		usleep(50000);
 	}
 	return 0;
 }
 
-void convert (vector<vector<int>> bufint){
-	for(int i = 0; i<height; i++){
-		for(int j = 0; j<width; j++){
-			buffer.at(j).at(i) = conv.at(bufint.at(j).at(i));
-		}
-	}
-}
-void charainit (void){
+/////////////////////////////////////////////////////
+
+
+void charainit (vector<vector<char>> &bufc){
 	printf("\x1b[H");
 	fflush(stdout);
 	for(int i = 0; i<width; i++){
 		for(int j = 0; j<height; j++){
-			buffer.at(i).at(j)=' ';
+			bufc.at(i).at(j)=' ';
 		}
 	}
 }
 
-void chara (vector<vector<char>> s){
+void chara (vector<vector<char>> &bufc){
 	for(int i = 0; i<height; i++){
 		for(int j = 0; j<width; j++){
 			if(j==width-1) printf("\n");
-			else putchar(s.at(j).at(i));
+			else putchar(bufc.at(j).at(i));
 
 			fflush(stdout);
+		}
+	}
+}
+
+void convert (vector<vector<int>> &bufint, vector<vector<char>> &bufc){
+	for(int i = 0; i<height; i++){
+		for(int j = 0; j<width; j++){
+			bufc.at(j).at(i) = conv.at(bufint.at(j).at(i));
 		}
 	}
 }
