be151289c7e634640ea7670cefce0a37c37cb493
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-03-04 14:13:46 +0900

    add makefile and linear function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <bits/stdc++.h>
+#include "linear.h"
 using namespace std;
 
 /////////////////////////////////////////////////////
-int width = 90;
-int height = 30;
+int width = 100;
+int height = 50;
 int frame = 100;
 vector<char> conv = {' ','I','X','W'};
 /////////////////////////////////////////////////////
@@ -31,21 +32,31 @@ int main(void){
 		convert(bufint,buffer);
 
 		chara(buffer);
-		usleep(90000);
+		usleep(9000);
 	}
+	vector<vector<int>> a ={
+		{1,2},
+		{3,4}};
+	vector<vector<int>> b ={
+		{1,2},
+		{3,4}};
+	vector<vector<int>> r = {2,vector<int>(2)};
+	mulp_two(a,b,r);
+	cout<<r.at(0).at(0)<<r.at(0).at(1)<<endl;
+	cout<<r.at(1).at(0)<<r.at(1).at(1)<<endl;
+
 	return 0;
 }
 
 /////////////////////////////////////////////////////
 
-
 void charainit (vector<vector<char>> &bufc, vector<vector<int>> &bufi){
 	printf("\x1b[H");
 	fflush(stdout);
-	for(int i = 0; i<width; i++){
-		for(int j = 0; j<height; j++){
-			bufc.at(i).at(j)=conv.at(0);
-			bufi.at(i).at(j)=0;
+	for(int i = 0; i<height; i++){
+		for(int j = 0; j<width; j++){
+			bufc.at(j).at(i)=conv.at(0);
+			bufi.at(j).at(i)=0;
 		}
 	}
 }
@@ -53,8 +64,8 @@ void charainit (vector<vector<char>> &bufc, vector<vector<int>> &bufi){
 void chara (vector<vector<char>> &bufc){
 	for(int i = 0; i<height; i++){
 		for(int j = 0; j<width; j++){
+			putchar(bufc.at(j).at(i));
 			if(j==width-1) printf("\n");
-			else putchar(bufc.at(j).at(i));
 
 			fflush(stdout);
 		}
